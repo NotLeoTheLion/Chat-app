2ed054b8be88f238fa0c98058ad0a44497605929
jgluk <NotLeoTheLion@>
2024-05-06 18:42:09 +0300

    first commit

---
+++ src/Client/Client/Client.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Client));
+            this.connectButton = new System.Windows.Forms.Button();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.localaddrLabel = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.logLabel = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.sendLabel = new System.Windows.Forms.Label();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.addrTextBox = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.Silver;
+            this.connectButton.FlatAppearance.BorderSize = 0;
+            this.connectButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connectButton.Font = new System.Drawing.Font("Montserrat Black", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(272, 46);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(4);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(116, 28);
+            this.connectButton.TabIndex = 28;
+            this.connectButton.TabStop = false;
+            this.connectButton.Text = "Prisijungti";
+            this.connectButton.UseVisualStyleBackColor = false;
+            this.connectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 3.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.portLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.portLabel.Location = new System.Drawing.Point(429, 36);
+            this.portLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(39, 18);
+            this.portLabel.TabIndex = 27;
+            this.portLabel.Text = "Port:";
+            this.portLabel.Click += new System.EventHandler(this.portLabel_Click);
+            // 
+            // localaddrLabel
+            // 
+            this.localaddrLabel.AutoSize = true;
+            this.localaddrLabel.BackColor = System.Drawing.Color.DimGray;
+            this.localaddrLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 3.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.localaddrLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.localaddrLabel.Location = new System.Drawing.Point(50, 36);
+            this.localaddrLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.localaddrLabel.Name = "localaddrLabel";
+            this.localaddrLabel.Size = new System.Drawing.Size(65, 18);
+            this.localaddrLabel.TabIndex = 26;
+            this.localaddrLabel.Text = "Adresas:";
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.BackColor = System.Drawing.Color.Silver;
+            this.portTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.portTextBox.Location = new System.Drawing.Point(478, 39);
+            this.portTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.portTextBox.MaxLength = 10;
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(132, 13);
+            this.portTextBox.TabIndex = 25;
+            this.portTextBox.TabStop = false;
+            this.portTextBox.Text = "9000";
+            this.portTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.Location = new System.Drawing.Point(17, 143);
+            this.logLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(49, 13);
+            this.logLabel.TabIndex = 31;
+            this.logLabel.Text = "Chat log:";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.Silver;
+            this.logTextBox.Location = new System.Drawing.Point(13, 164);
+            this.logTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logTextBox.Size = new System.Drawing.Size(568, 301);
+            this.logTextBox.TabIndex = 30;
+            this.logTextBox.TabStop = false;
+            // 
+            // sendLabel
+            // 
+            this.sendLabel.AutoSize = true;
+            this.sendLabel.BackColor = System.Drawing.Color.Transparent;
+            this.sendLabel.Font = new System.Drawing.Font("Montserrat Black", 15.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.sendLabel.Location = new System.Drawing.Point(196, 472);
+            this.sendLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.sendLabel.Name = "sendLabel";
+            this.sendLabel.Size = new System.Drawing.Size(192, 29);
+            this.sendLabel.TabIndex = 33;
+            this.sendLabel.Text = "Issiusti zinutes:";
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.BackColor = System.Drawing.Color.Silver;
+            this.sendTextBox.Location = new System.Drawing.Point(13, 509);
+            this.sendTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.sendTextBox.Multiline = true;
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.sendTextBox.Size = new System.Drawing.Size(568, 71);
+            this.sendTextBox.TabIndex = 32;
+            this.sendTextBox.TabStop = false;
+            this.sendTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.SendTextBox_KeyDown);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Silver;
+            this.clearButton.FlatAppearance.BorderSize = 0;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearButton.Font = new System.Drawing.Font("Montserrat Black", 8.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(610, 289);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(116, 28);
+            this.clearButton.TabIndex = 34;
+            this.clearButton.TabStop = false;
+            this.clearButton.Text = "Isvalyti";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 3.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.usernameLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.usernameLabel.Location = new System.Drawing.Point(26, 65);
+            this.usernameLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(89, 18);
+            this.usernameLabel.TabIndex = 36;
+            this.usernameLabel.Text = "Slapyvardis:";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.BackColor = System.Drawing.Color.Silver;
+            this.usernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.usernameTextBox.Location = new System.Drawing.Point(123, 70);
+            this.usernameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.usernameTextBox.MaxLength = 50;
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(132, 13);
+            this.usernameTextBox.TabIndex = 35;
+            this.usernameTextBox.TabStop = false;
+            this.usernameTextBox.Text = "Client";
+            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Font = new System.Drawing.Font("Montserrat SemiBold", 3.249999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Millimeter, ((byte)(0)));
+            this.keyLabel.ForeColor = System.Drawing.Color.Gainsboro;
+            this.keyLabel.Location = new System.Drawing.Point(410, 65);
+            this.keyLabel.Margin = new System.Windows.Forms.Padding(8, 4, 4, 4);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(58, 18);
+            this.keyLabel.TabIndex = 38;
+            this.keyLabel.Text = "Raktas:";
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.BackColor = System.Drawing.Color.Silver;
+            this.keyTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.keyTextBox.Location = new System.Drawing.Point(476, 68);
+            this.keyTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.keyTextBox.MaxLength = 200;
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(132, 13);
+            this.keyTextBox.TabIndex = 37;
+            this.keyTextBox.TabStop = false;
+            this.keyTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // addrTextBox
+            // 
+            this.addrTextBox.BackColor = System.Drawing.Color.Silver;
+            this.addrTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.addrTextBox.Location = new System.Drawing.Point(123, 39);
+            this.addrTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.addrTextBox.MaxLength = 200;
+            this.addrTextBox.Name = "addrTextBox";
+            this.addrTextBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.addrTextBox.Size = new System.Drawing.Size(132, 13);
+            this.addrTextBox.TabIndex = 39;
+            this.addrTextBox.TabStop = false;
+            this.addrTextBox.Text = "127.0.0.1";
+            this.addrTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox
+            // 
+            this.checkBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBox.Location = new System.Drawing.Point(496, 93);
+            this.checkBox.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(72, 20);
+            this.checkBox.TabIndex = 41;
+            this.checkBox.Text = "Hide key";
+            this.checkBox.UseVisualStyleBackColor = true;
+            this.checkBox.CheckedChanged += new System.EventHandler(this.CheckBox_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-58, -86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(754, 308);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 42;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Client
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(739, 613);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.addrTextBox);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.sendLabel);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.logLabel);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.localaddrLabel);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.Label localaddrLabel;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label logLabel;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Label sendLabel;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.TextBox addrTextBox;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
